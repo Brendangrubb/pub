b64fcddc0ff3b2f32b4541def90b55742412345b
Brendan Grubb and Benjamin T. Seaver <brendan@>
2017-03-29 16:41:00 -0700

    Add seed data and keg list table

--- src/app/keg-list/keg-list.component.ts
+++ src/app/keg-list/keg-list.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Keg } from './../keg';
 
+
 @Component({
   selector: 'app-keg-list',
   templateUrl: './keg-list.component.html',
   styleUrls: ['./keg-list.component.css']
 })
+
+
 export class KegListComponent implements OnInit {
-  kegList: Keg[] = [];
+  kegList: Keg[] = [
+    new Keg("Squirrel IPA", "SheDoots", 5, 7.5),
+    new Keg("Hop Scotch Red", "Pearl Divers", 4, 7.1),
+    new Keg("Big Stick Stout", "East Side Brewlers", 6, 8, 120)
+  ];
 
   constructor() { }
 
